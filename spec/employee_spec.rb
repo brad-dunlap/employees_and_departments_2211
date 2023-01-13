035f4ae8b10fd13be9e28f19afd720173f2a4531
Brad Dunlap <braddunlapdpt@gmail.com>
2023-01-13 11:09:33 -0500

    feat: Testing for #give_raise

--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -16,4 +16,11 @@ RSpec.describe Employee do
 			expect(bobbi.salary).to eq(100000)
 		end
 	end
+
+	describe '#give_raise' do
+		it 'adds a raise to current salary' do
+			bobbi.give_raise(5000)
+			expect(bobbi.salary).to eq(105000)
+		end
+	end
 end
