de7295e803a76a464e8cea241e239fc609f9b7b9
Brad Dunlap <braddunlapdpt@gmail.com>
2023-01-13 11:32:13 -0500

    feat: test for creation of Budget class with corrected attributes

--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -8,15 +8,15 @@ RSpec.describe Budget do
 		let(:customer_service) { Department.new("Customer Service") }
 		let(:bobbi) { Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"}) }
 		let(:aaron) { Employee.new({name: "Aaron Tanaka", age: "25", salary: "$90000"}) }
-		let(:budget) { Budget.new({year: 2022, department: customer_service})}
+		let(:budget) { Budget.new({year: 2022, departments: customer_service})}
 	
 	describe '#initialize' do
 		it 'exists' do
-			expect(customer_service).to be_instance_of(Department)
+			expect(budget).to be_instance_of(Budget)
 		end
 
 		it 'has attributes' do
-			expect(customer_service.name).to eq("Customer Service")
-			expect(customer_service.employees).to eq([])
+			expect(budget.year).to eq(2022)
+			expect(budget.departments).to eq([customer_service])
 		end
 	end
